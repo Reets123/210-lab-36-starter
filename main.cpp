0e76bc83466a023ebe37ac5979c8605c0ab48ead
Reets123 <ibrahimalatig@gmail.com>
2024-11-27 11:38:44 +0000

    commit 7

--- main.cpp
+++ main.cpp
@@ -2,9 +2,23 @@
 // COMSC-210 - Ibrahim Alatig 
 
 #include <iostream>
+#include <fstream>
 #include "IntBinaryTree.h"
 
 using namespace std;
+
+void loadFromFile(IntBinaryTree &tree, const string &filename) {
+    ifstream file(filename);
+    if (!file) {
+        cout << "Error opening file. Check files name" << endl;
+        return;
+    }
+
+     string code;
+    while (file >> code) {
+        tree.insertNode(code);
+    }
+    file.
 
 int main() {
     
