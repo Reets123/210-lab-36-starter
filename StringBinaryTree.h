43089135c43db1f37bc6bc43c6df576b586b35b2
Reets123 <ibrahimalatig@gmail.com>
2024-11-29 07:50:56 +0000

    commit 16

--- StringBinaryTree.h
+++ StringBinaryTree.h
@@ -5,36 +5,34 @@
 #define STRINGBINARYTREE_H
 
 #include <string>
+#include <iostream>
 using namespace std;
 
 // The StringBinaryTree class manages a binary tree of strings.
 class StringBinaryTree {
 private:
-   // TreeNode is a private structure for StringBinaryTree nodes.
-   struct TreeNode {
-      string value;         // The value in the node
-      TreeNode *left;      // Pointer to left child node
-      TreeNode *right;     // Pointer to right child node
-   };
+    struct TreeNode {
+        string value;         // The value in the node
+        TreeNode *left;      // Pointer to left child node
+        TreeNode *right;     // Pointer to right child node
+    };
 
-   TreeNode *root;      
+    TreeNode *root;
 
-   // Private member functions for internal operations.
-   void insert(TreeNode *&, TreeNode *&);
-   void destroySubTree(TreeNode *);
-   void deleteNode(const string&, TreeNode *&);
-   void makeDeletion(TreeNode *&);
-   void displayInOrder(TreeNode *) const;
+    void insert(TreeNode *&, TreeNode *&);
+    void destroySubTree(TreeNode *);
+    void deleteNode(const string&, TreeNode *&);
+    void makeDeletion(TreeNode *&);
+    void displayInOrder(TreeNode *) const;
 
 public:
-   StringBinaryTree()     { root = nullptr; }
-   ~StringBinaryTree()    { destroySubTree(root); }
+    StringBinaryTree() : root(nullptr) {}
+    ~StringBinaryTree() { destroySubTree(root); }
 
-   void insertNode(const string&);
-   bool searchNode(const string&);
-   void remove(const string&);
-
-   void displayInOrder() const     {  displayInOrder(root); }
+    void insertNode(const string&);
+    bool searchNode(const string&);
+    void remove(const string&);
+    void displayInOrder() const { displayInOrder(root); }
 };
 
 #endif // STRINGBINARYTREE_H
