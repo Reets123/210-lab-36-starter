f06e8b2f99bd87af4f00cb0502576706cebe41e5
Reets123 <ibrahimalatig@gmail.com>
2024-11-29 06:03:19 +0000

    commit 12

--- IntBinaryTree.h
+++ IntBinaryTree.h
@@ -1,24 +1,22 @@
 // Lab 36: Records BST
 // COMSC-210 - Ibrahim Alatig 
 
-// Specification file for the IntBinaryTree class
-#ifndef INTBINARYTREE_H
-#define INTBINARYTREE_H
+#ifndef STRINGBINARYTREE_H
+#define STRINGBINARYTREE_H
 
 #include <string>
 using namespace std;
 
-// The IntBinaryTree class manages a binary tree of integers.
-class IntBinaryTree {
+// The StringBinaryTree class manages a binary tree of strings.
+class StringBinaryTree {
 private:
-   // TreeNode is a private structure for IntBinaryTree nodes.
+   // TreeNode is a private structure for StringBinaryTree nodes.
    struct TreeNode {
       string value;         // The value in the node
-      TreeNode *left;    // Pointer to left child node
-      TreeNode *right;   // Pointer to right child node
+      TreeNode *left;      // Pointer to left child node
+      TreeNode *right;     // Pointer to right child node
    };
 
-   // root points to the topmost node of the tree.
    TreeNode *root;      
 
    // Private member functions for internal operations.
@@ -27,25 +25,16 @@ private:
    void deleteNode(const string&, TreeNode *&);
    void makeDeletion(TreeNode *&);
    void displayInOrder(TreeNode *) const;
-   void displayPreOrder(TreeNode *) const;
-   void displayPostOrder(TreeNode *) const;
 
 public:
-   // Constructor initializes the root to nullptr, indicating an empty tree.
-   IntBinaryTree()     { root = nullptr; }
+   StringBinaryTree()     { root = nullptr; }
+   ~StringBinaryTree()    { destroySubTree(root); }
 
-   // Destructor deallocates all nodes in the tree.
-   ~IntBinaryTree()    { destroySubTree(root); }
-
-   // Public interface for inserting, searching, and removing nodes.
    void insertNode(const string&);
    bool searchNode(const string&);
    void remove(const string&);
 
-   // Public wrappers for tree traversal functions.
    void displayInOrder() const     {  displayInOrder(root); }
-   void displayPreOrder() const    {  displayPreOrder(root); }
-   void displayPostOrder() const   {  displayPostOrder(root); }
 };
 
-#endif // INTBINARYTREE_H
+#endif // STRINGBINARYTREE_H
